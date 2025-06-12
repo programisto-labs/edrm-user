1fad1ee215d511d663020170b744471db7ddf84f
Grégoire Falquerho <g.falquerho@programisto.fr>
2025-06-12 16:52:55 +0200

    fix: add test for build

---
+++ test/user.test.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-expressions */
+import { expect } from 'chai';
+
+describe('Basic Sanity Check', () => {
+  it('should always pass', () => {
+    expect(true).to.be.true;
+  });
+});
